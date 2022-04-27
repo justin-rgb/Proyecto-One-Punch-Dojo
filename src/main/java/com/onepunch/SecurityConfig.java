dd686caa3742ac38907e565d1c690642e10fdd26
Justin Varela C <justinvaco07@gmail.com>
2022-04-27 17:04:59 -0600

    Se ajusta la configuracion de Seguridad y la ventana del Error 403

--- src/main/java/com/onepunch/SecurityConfig.java
+++ src/main/java/com/onepunch/SecurityConfig.java
@@ -15,15 +15,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
         
         auth.inMemoryAuthentication()
-                .withUser("justin")
+        .withUser("justin")
                 .password("{noop}123")
-                .roles("ADMIN","VENDEDOR","USER")
+                .roles("ADMIN","ENTRENADOR","USUARIO")
         .and()
         .withUser("julian")
                 .password("{noop}123")
-                .roles("ENTRENADOR","USUARIO")
+                .roles("ADMIN","ENTRENADOR","USUARIO")
         .and()
         .withUser("profe")
+                .password("{noop}123")
+                .roles("ENTRENADOR","USUARIO")
+        .and()
+        .withUser("usuario")
                 .password("{noop}123")
                 .roles("USUARIO");
     }
@@ -32,6 +36,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http.authorizeRequests()
+                
+            .antMatchers("/clientes/modificar/**","/clientes/eliminar/**",
+                        "/colaboradores/modificar/**","/colaboradores/eliminar/**",
+                        "/compras/modificar/**","/compras/eliminar/**",
+                        "/facturas/modificar/**","/facturas/eliminar/**",
+                        "/eventos/modificar/**","/eventos/eliminar/**",
+                        "/proveedores/modificar/**","/proveedores/eliminar/**",
+                        "/trofeos/modificar/**","/trofeos/eliminar/**",
+                        "/inventario/modificar/**","/inventario/eliminar/**"
+                    ).hasRole("ADMIN")
+  
+            .antMatchers("/clientes/nuevo","/clientes/guardar",
+                        "/colaboradores/nuevo","/colaboradores/guardar",
+                        "/compras/nuevo","/compras/guardar",
+                        "/facturas/nuevo","/facturas/guardar",
+                        "/eventos/nuevo","/eventos/guardar",
+                        "/proveedores/nuevo","/proveedores/guardar",
+                        "/trofeos/nuevo","/trofeos/guardar",
+                        "/inventario/nuevo","/inventario/guardar"
+                    ).hasAnyRole("ADMIN","ENTRENADOR")
+                
+                
             .antMatchers("/").hasAnyRole("ENTRENADOR","USUARIO","ADMIN")
             
             .antMatchers("/admin*").access("hasRole('ADMIN')")
