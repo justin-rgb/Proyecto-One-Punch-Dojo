358873c04f4ccdb7493ee9e33020159d2418233d
jubargra <65262373+jubargra@users.noreply.github.com>
2022-04-26 22:00:27 -0600

    commit ''manual'' trofeDao
    Netbeans no me deja hacer pull entonces no puedo hacer push

---
+++ src/main/java/com/onepunch/dao/TrofeoDao.java
@@ -0,0 +1,8 @@
+package com.onepunch.dao;
+
+import com.onepunch.domain.Trofeo;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TrofeoDao extends CrudRepository<Trofeo, Long> {
+    
+}
